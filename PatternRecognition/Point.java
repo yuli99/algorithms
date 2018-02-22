a23711dd73b64f44c11976680e717956e4fb1d0c
yuli99 <yuliwei99@gmail.com>
2018-02-22 09:43:23 -0500

    Update Point.java

--- PatternRecognition/Point.java
+++ PatternRecognition/Point.java
@@ -2,7 +2,7 @@
  * Name:  Yuli Wei
  *
  * Algorithm Project: Pattern Recognition
- * Using stdlib.jar, which is part of the algs4.jar
+ * Using stdlib.jar
  *
  * Compilation:  javac Point.java
  * Execution:
